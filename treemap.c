066bae19787ac9d145fd1f1ccde66d73491b019f
Your Name <you@example.com>
2026-04-30 20:53:42 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -110,13 +110,11 @@ Pair * firstTreeMap(TreeMap * tree) {
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
-    if (tree->current->right == NULL && tree->lower_than(tree->current->pair->key,
-        tree->current->parent->pair->key) {
+    if (tree->current->right == NULL && tree->lower_than(tree->current->pair->key, tree->current->parent->pair->key) {
         return tree->current->parent->pair;
     }
 
-    if (tree->lower_than(tree->current->parent->key, 
-        tree->current->pair->key) && tree->current->right == NULL) {
+    if (tree->lower_than(tree->current->parent->key, tree->current->pair->key) && tree->current->right == NULL) {
         return NULL;
         }
     /* TreeNode *padre = tree->current->parent;
